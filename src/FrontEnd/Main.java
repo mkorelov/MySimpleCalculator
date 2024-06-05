2f59a32c5a507684a370305071ec7528df06be73
Michael Korelov <146900945+mkorelov@users.noreply.github.com>
2024-06-05 16:04:32 -0700

    fixed javafx missing error

--- src/FrontEnd/Main.java
+++ src/FrontEnd/Main.java
@@ -2,12 +2,17 @@ package FrontEnd;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) {
-
+    public void start(Stage stage) {
+        PrimaryFrame root = new PrimaryFrame();
+        stage.setTitle("Calculator");
+        stage.setScene(new Scene(root, 500, 600));
+        stage.setResizable(false);
+        stage.show();
     }
 
     public static void main(String[] args) throws Exception {
