18dad5f50e18999a5d4cab2b9caa65123f5fb06f
Michael Korelov <146900945+mkorelov@users.noreply.github.com>
2024-06-07 10:01:53 -0700

    initialized primary buttons

--- src/FrontEnd/PrimaryScreen.java
+++ src/FrontEnd/PrimaryScreen.java
@@ -5,7 +5,7 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.control.Button;
 
-class PrimaryScreen extends VBox{
+class PrimaryScreen extends VBox {
     private Button clear;
     private Button negative;
     private Button decimal;
@@ -30,7 +30,28 @@ class PrimaryScreen extends VBox{
     private Button more;
 
     PrimaryScreen() {
-    
+        //this.setPrefSize();
+        //this.setStyle();
+
+        clear = new Button("AC");
+        negative = new Button("+/-");
+        decimal = new Button(".");
+        plus = new Button("+");
+        minus = new Button("-");
+        times = new Button("x");
+        divide = new Button("/");
+        equals = new Button("=");
+
+        zero = new Button("0");
+        one = new Button("1");
+        two = new Button("2");
+        three = new Button("3");
+        four = new Button("4");
+        five = new Button("5");
+        six = new Button("6");
+        seven = new Button("7");
+        eight = new Button("8");
+        nine = new Button("9");
     }
 }
 
